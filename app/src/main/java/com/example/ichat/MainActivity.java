3df801bd5239c8c93670470a45b185c1272dac49
Shivendra Pratap Singh <76787980+shivaay0706@users.noreply.github.com>
2021-10-12 16:55:40 +0530

    newiChatMessenger

--- app/src/main/java/com/example/ichat/MainActivity.java
+++ app/src/main/java/com/example/ichat/MainActivity.java
@@ -1,14 +1,57 @@
 package com.example.ichat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.ichat.R.id;
+import com.example.ichat.databinding.ActivityMainBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        auth = FirebaseAuth.getInstance();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu , menu);
+
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case id.settings:
+                Toast.makeText(this, "Setting Clicked", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.Logout:
+                auth.signOut();
+                Intent intent = new Intent(MainActivity.this , SignInActivity.class);
+                startActivity(intent);
+                break;
+        }
+        return true;
     }
 }
